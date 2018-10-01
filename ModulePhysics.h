eb5e0987038f21dc74070683605efa217e5b83b4
ponspack9 <ospoga9@gmail.com>
2018-10-01 10:18:14 +0200

    Hello world
    Draws a static circle somewhere in the screen

---
+++ ModulePhysics.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+
+class b2World;
+
+class ModulePhysics : public Module
+{
+public:
+	ModulePhysics(Application* app, bool start_enabled = true);
+	~ModulePhysics();
+
+	bool Start();
+	update_status PreUpdate();
+	update_status PostUpdate();
+	bool CleanUp();
+
+private:
+
+	b2World *world;
+	bool debug;
+};
